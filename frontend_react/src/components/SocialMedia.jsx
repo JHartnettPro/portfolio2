c118bbb250d98c04091cfb7abd091a9b8773a80e
John Hartnett <jhartnettpro@gmail.com>
2022-03-16 16:52:09 -0500

    basically finished need to add more projects and deploy them

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,16 +5,20 @@ const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
+          <a href="https://www.linkedin.com/in/john-hartnett-24b2261b7/">
             <BsLinkedin/>
+            </a>
         </div>
         <div>
-        <a href="https://google.com" >
+        <a href="https://stackoverflow.com/users/18477028/jhartnettpro" >
             <BsStackOverflow/>
             </a>
         </div>
          
         <div>
+          <a href="https://github.com/JHartnettPro">
             <BsGithub/>
+            </a>
         </div>
     </div>
   )
